c5547adc397d4d118f945ce3cf948c6a5b3ab438
hryu <ryu.snu@gmail.com>
2017-03-29 20:09:47 +0900

    initial commit :)

---
+++ FastcampusFrameHD/FastcampusFrameHD.h
@@ -0,0 +1,19 @@
+//
+//  FastcampusFrameHD.h
+//  FastcampusFrameHD
+//
+//  Created by Ryu Hyun Do on 2017. 3. 29..
+//  Copyright © 2017년 hyundo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FastcampusFrameHD.
+FOUNDATION_EXPORT double FastcampusFrameHDVersionNumber;
+
+//! Project version string for FastcampusFrameHD.
+FOUNDATION_EXPORT const unsigned char FastcampusFrameHDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FastcampusFrameHD/PublicHeader.h>
+
+
